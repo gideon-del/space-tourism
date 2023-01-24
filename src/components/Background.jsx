36004959345470a4d6d531561862fe76c6956adf
gideon-del <gideonchidi471@gmial.com>
2023-01-24 16:18:26 +0100

    Done

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,15 +3,27 @@ import React from "react";
 
 const Background = ({ mobile, tablet, desk }) => {
   return (
-    <picture className="fixed w-screen h-screen -z-10">
-      <source srcSet={mobile.src} media="(max-width: 600px)" />
-      <source srcSet={tablet.src} media="(max-width: 1200px)" />
-      <img
-        src={desk.src}
-        className="w-full h-full object-center"
-        alt="Destination"
-      />
-    </picture>
+    <style jsx global>
+      {`
+        body {
+          background-image: url(${mobile.src});
+          background-repeat: no-repeat;
+          background-size: cover;
+          background-position: center bottom;
+        }
+        @media (min-width: 35em) {
+          body {
+            background-image: url(${tablet.src});
+          }
+        }
+
+        @media (min-width: 64em) {
+          body {
+            background-image: url(${desk.src});
+          }
+        }
+      `}
+    </style>
   );
 };
 
